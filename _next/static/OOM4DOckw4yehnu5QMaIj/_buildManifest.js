21d7875bb3997e447749692a7bad4601f8f9f348
Puthnith Var <18649570+puthnith@users.noreply.github.com>
2021-02-26 17:53:55 +0000

    chore: create Next.js app (70deec6)

---
+++ _next/static/OOM4DOckw4yehnu5QMaIj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-afa1f8de2921572413f1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1c21c8a129ce2492c717.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
